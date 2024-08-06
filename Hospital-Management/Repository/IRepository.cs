3662c29c219b832fd884c35691f70cfb59207c9b
Mohamed Kamal <ku18mz@gmail.com>
2024-08-06 21:17:58 +0300

    Create normal entities generic Repo interface

---
+++ Hospital-Management/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Hospital_Management.Repository
+{
+    public interface IRepository<Entity>
+    {
+        public List<Entity> GetAll();
+        public List<Entity> GetPage(int page);
+        public int GetTotalPages(int pageSize);
+        public List<Entity> Search(string searchString, string searchProperty);
+        public Entity GetById(int id);
+        public void Insert(Entity entity);
+        public void Update(Entity entity);
+        public void Delete(int id);
+        public void Save();
+    }
+}
