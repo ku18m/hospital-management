2b30ef44557d32418b8318f6965ae91247c1bf0e
hazem mohamed salah <hazem.salah16299@gmail.com>
2024-08-10 00:06:27 +0300

    creating profile controller

---
+++ Hospital-Management/Controllers/ProfileController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Hospital_Management.ViewModels;
+using Hospital_Management.Models;
+using Hospital_Management.Repository;
+using Hospital_Management.Services;
+
+namespace Hospital_Management.Controllers
+{
+    public class ProfileController : Controller
+    {
+        
+
+        public ProfileController(UserServices)
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            var model = new UserProfileData
+            {
+                UserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                UserName = User.Identity.Name,
+                Email = User.FindFirst(ClaimTypes.Email)?.Value,
+                FirstName = User.FindFirst(ClaimTypes.GivenName)?.Value,
+                LastName = User.FindFirst(ClaimTypes.Surname)?.Value,
+                BirthDate = User.FindFirst(ClaimTypes.DateOfBirth)?.Value,
+            };
+            return View(model);
+        }
+        public async Task<IActionResult> SaveChangesAsync(UserProfileData model, IFormFile Img)
+        {
+            if (ModelState.IsValid)
+            {
+                // Handle the profile picture upload
+                if (Img != null && Img.Length > 0)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await Img.CopyToAsync(memoryStream);
+                        model.Img = memoryStream.ToArray();
+                    }
+                  
+                }
+
+            }
+           
+           
+            return View(model);
+        }
+    }
+}
---
+++ Hospital-Management/ViewModels/UserProfileData.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital_Management.ViewModels
+{
+    public class UserProfileData
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public byte[]? Img { get; set; }
+    }
+}
