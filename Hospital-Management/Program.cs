310f9c3fad612b4978b6f8adc97097ff041c3017
Mohamed Kamal <ku18mz@gmail.com>
2024-08-11 19:14:38 +0300

    Data seeding is done, tested Works correctly with all relationships

--- Hospital-Management/Program.cs
+++ Hospital-Management/Program.cs
@@ -117,19 +117,19 @@ namespace Hospital_Management
 
 
             // Call DataSeeder.
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    await DataSeeder.SeedData(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    try
+            //    {
+            //        await DataSeeder.SeedData(services);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred seeding the DB.");
+            //    }
+            //}
 
             app.Run();
         }
